d355bdfa78f9a837dc69340bba3448cbf5bc98d5
Mike <29874229+bearmannl@users.noreply.github.com>
2017-10-31 20:45:59 +0100

    Minor style changes

--- AutoSPInstallerConfigureRemoteTarget.ps1
+++ AutoSPInstallerConfigureRemoteTarget.ps1
@@ -1,16 +1,13 @@
 ﻿# Configures the server for WinRM and WSManCredSSP
 Write-Host "Configuring PowerShell remoting..."
-$winRM = Get-Service -Name winrm
-If ($winRM.Status -ne "Running") {Start-Service -Name winrm}
+$winRM = Get-Service -Name WinRM
+If ($winRM.Status -ne "Running") {Start-Service -Name WinRM}
 Set-ExecutionPolicy Bypass -Force
 Enable-PSRemoting -Force
-Enable-WSManCredSSP -Role Server -Force | Out-Null
+Enable-WSManCredSSP -Role Server -Force > $null
+
 # Increase the local memory limit to 1 GB
 Set-Item WSMan:\localhost\Shell\MaxMemoryPerShellMB 1024
 
 #Get out of this PowerShell process
 Stop-Process -Id $PID -Force
-
-
-
-
