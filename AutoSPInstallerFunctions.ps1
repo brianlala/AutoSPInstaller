f6666ace4bf1f94cf4da656152db54a308371c20
lknr <lknreg@gmail.com>
2018-02-14 17:36:06 +0100

    fixes #17

--- AutoSPInstallerFunctions.ps1
+++ AutoSPInstallerFunctions.ps1
@@ -7703,7 +7703,7 @@ Function Stop-DefaultWebsite ()
     # Added to avoid conflicts with web apps that do not use a host header
     # Thanks to Paul Stork per http://autospinstaller.codeplex.com/workitem/19318 for confirming the Stop-Website cmdlet
     ImportWebAdministration
-    $defaultWebsite = Get-Website | Where-Object {$_.Name -eq "Default Web Site" -or $_.ID -eq 1 -or $_.physicalPath -eq "%SystemDrive%\inetpub\wwwroot"} # Try different ways of identifying the Default Web Site, in case it has a different name (e.g. localized installs)
+    $defaultWebsite = Get-Website | Where-Object {$_.Name -eq "Default Web Site" -or $_.physicalPath -eq "%SystemDrive%\inetpub\wwwroot"} # Try different ways of identifying the Default Web Site, in case it has a different name (e.g. localized installs)
     Write-Host -ForegroundColor White " - Checking $($defaultWebsite.Name)..." -NoNewline
     if ($defaultWebsite.State -ne "Stopped")
     {
